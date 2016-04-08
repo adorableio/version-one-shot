3b2a2e7816f6e304e137983cd104ef59898bdaf8
jremsik <jim@adorable.io>
2016-04-07 22:37:42 -0500

    Load options from Chrome storage

--- app/scripts.babel/options.js
+++ app/scripts.babel/options.js
@@ -1,3 +1,35 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Option');
+console.log('Make VersionOne more adorable');
+
+function restore_options() {
+  chrome.storage.sync.get({
+    columnsToHide: '1057, 137, 1063',
+    epicsToHide: '197443,207470'
+  }, function(items) {
+    document.getElementById('columns').value = items.columnsToHide;
+    document.getElementById('epics').value = items.epicsToHide;
+  });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+
+function save_options() {
+  var columns = document.getElementById('columns').value;
+  console.log(`columns: ${columns}`)
+  var epics = document.getElementById('epics').value;
+  console.log(`epics: ${epics}`)
+
+  chrome.storage.sync.set({
+    columnsToHide: columns,
+    epicsToHide: epics
+  }, function() {
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 750);
+  });
+}
+
+document.getElementById('save').addEventListener('click', save_options);

108c97ec4b8659dcd8856f7a7e4ee86074b2a014
Casey Brant <casey@caseybrant.com>
2016-05-07 09:51:42 -0500

    set checked status of hide epics checkbox based on storage

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,6 +1,12 @@
 'use strict';
 
-console.log('hi');
+document.addEventListener('DOMContentLoaded', function() {
+  var hide_epics_checkbox = document.getElementById('toggle-cards');
+  chrome.storage.sync.get({ hideEpicCards: true }, function(items) {
+    hide_epics_checkbox.checked = items.hideEpicCards;
+  });
+
+});
 
 document.getElementById('toggle-cards').addEventListener('change', function(e) {
   chrome.storage.sync.set({

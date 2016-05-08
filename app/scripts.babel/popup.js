5331ac4d120a59c934d1cbdd6cd8fef91c92ef88
Casey Brant <casey@caseybrant.com>
2016-05-07 21:52:13 -0500

    show the epics list in the popup (doesn't save changes yet)

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,14 +1,34 @@
 'use strict';
 
+var hide_epics_checkbox = document.getElementById('toggle-cards'),
+    epics_list =          document.getElementById('epics-list');
+
+
 document.addEventListener('DOMContentLoaded', function() {
-  var hide_epics_checkbox = document.getElementById('toggle-cards');
   chrome.storage.sync.get({ hideEpicCards: true }, function(items) {
-    hide_epics_checkbox.checked = items.hideEpicCards;
+    if (items.hideEpicCards) {
+      hide_epics_checkbox.checked = true;
+      toggle_epics_list(true);
+    } else {
+      toggle_epics_list(false);
+    }
   });
 });
 
 document.getElementById('toggle-cards').addEventListener('change', function(e) {
+  toggle_epics_list(e.target.checked);
   chrome.storage.sync.set({
     'hideEpicCards': e.target.checked
   });
 });
+
+function toggle_epics_list(show_it) {
+  if (show_it) {
+    chrome.storage.sync.get({ columnsToHide: '' }, function(items) {
+      epics_list.value = items.columnsToHide;
+    });
+    epics_list.classList.remove('hidden');
+  } else {
+    epics_list.classList.add('hidden');
+  }
+}

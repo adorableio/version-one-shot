cdaf0a2b89d4989dbaa3744acf2acb5a659da4b8
jremsik <jim@adorable.io>
2016-05-13 21:42:58 -0500

    Set the columns and epics to hide on install

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -2,6 +2,10 @@
 
 chrome.runtime.onInstalled.addListener(details => {
   console.log('previousVersion', details.previousVersion);
+  chrome.storage.sync.set({
+    columnsToHide: '137, 1057, 1063',
+    epicsToHide: '213682'
+  });
 });
 
 chrome.tabs.onUpdated.addListener(tabId => {
